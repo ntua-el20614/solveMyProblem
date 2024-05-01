21804ea4aace51f5f07b675d6a97ad9cbad6bb67
el20614 <el20614@mail.ntua.gr>
2024-05-02 00:11:31 +0300

    dockers

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./app');
 
-const PORT = process.env.PORT || 7007;
+const PORT = process.env.PORT || 8448;
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}!\n🚀 http://localhost:${PORT} 🚀`));
