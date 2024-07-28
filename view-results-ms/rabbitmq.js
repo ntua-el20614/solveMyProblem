dc013076dd018220595813871c3bd04f443309c2
ntua-el20605 <el20605@mail.ntua.gr>
2024-07-28 16:23:29 +0300

    Removed unused code

--- view-results-ms/rabbitmq.js
+++ view-results-ms/rabbitmq.js
@@ -4,36 +4,6 @@ const SolvedProblems = require('./Models/Results');
 
 let channel = null;
 const rabbitURI = process.env.RABBITMQ_URL;
-//const queueName='solvedProblems';
-
-/*exports.consumeFromQueue = (queueName) => {
-    if (!channel) {
-      console.error('Channel not set. Call connectRabbitMQ() first.');
-      return;
-    }
-    channel.assertQueue(queueName, { durable: true });
-    channel.consume(queueName, async (msg) => {
-      if (msg !== null) {
-        try {
-          const messageContent = msg.content.toString();
-          console.log("Received message:", messageContent);
-  
-          // Parse the message content
-          const data = JSON.parse(messageContent);
-          
-          // Save data to MongoDB
-          const newSolvedProblem = new SolvedProblems({ output_file: data.output_file, status: 'solved', createdBy: data.createdBy });
-          await newSolvedProblem.save();
-          console.log("Result saved to MongoDB");
-        } catch (error) {
-          console.error("Error processing message:", error);
-        }
-        
-        // Acknowledge the message
-        channel.ack(msg);
-      }
-    });
-  };*/
 
 exports.connectRabbitMQ = (retries = 5) => {
   amqp.connect(rabbitURI, (err, conn) => {
@@ -67,14 +37,3 @@ exports.connectRabbitMQ = (retries = 5) => {
     });
   });
 };
-
-/*exports.publishToQueue = (queueName, message) => {
-  if (!channel) {
-    console.error('Channel not set. Call connectRabbitMQ() first.');
-    return;
-  }
-  channel.assertQueue(queueName, { durable: true });
-  channel.sendToQueue(queueName, Buffer.from(message));
-};*/
-
-
