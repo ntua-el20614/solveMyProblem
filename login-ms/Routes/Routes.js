adb624efa79706ec27684c830ea2915f2067b434
GeorgeOLoco <gpittakis@gmail.com>
2024-08-23 16:37:39 +0300

    Added endpoint for the credits

--- login-ms/Routes/Routes.js
+++ login-ms/Routes/Routes.js
@@ -9,4 +9,9 @@ router.post('/logout', Controller.logout);
 router.get('/users', Controller.getUsers);
 router.get('/health', Controller.getHealth);
 
+router.get('/view_temp_credits/:username', Controller.viewTempCredits);
+router.get('/view_credits/:username', Controller.viewCredits);
+
+router.post('/change_temp_credits/:username/:value', Controller.addTempCredits);
+router.post('/change_credits/:username/:value', Controller.addCredits);
 module.exports = router;
